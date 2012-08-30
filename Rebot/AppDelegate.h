78cc01edcdc4b900c2d295d54c1704d6715272a5
Chuankai Lin <chuankai@kilab.tw>
2012-08-31 02:34:02 +0800

    Initial Commit

---
+++ Rebot/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Rebot
+//
+//  Created by Lin Chuankai on 8/31/12.
+//  Copyright (c) 2012 KILAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Rebot/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Rebot
+//
+//  Created by Lin Chuankai on 8/31/12.
+//  Copyright (c) 2012 KILAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
